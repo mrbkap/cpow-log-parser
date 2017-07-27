b55080b5ce4af39569cb6a170dd76e4061badabf
Blake Kaplan <mrbkap@gmail.com>
2017-07-26 17:29:06 -0700

    Don't copy the file into a huge array, stream it

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ extern crate nom;
 use nom::{IResult, not_line_ending, digit, hex_digit};
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+use std::iter;
 use std::collections::BTreeMap;
 use std::str::{self, FromStr};
 use clap::App;
 
 struct Parser {
+    input_stream: Box<Iterator<Item=String>>,
 }
 
 #[derive(Debug, PartialEq, Eq, Hash)]
@@ -38,22 +40,38 @@ struct Test {
     indirect_cpows: Vec<IndirectCPOW>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum LogLine {
     TestStart(String),
     StackComponent(u32, String, String, u32),
 }
 
 impl Parser {
-    fn new() -> Parser {
-        Parser { }
+    fn new(fname: &str) -> Parser {
+        let reader : Box<io::Read> = if fname != "-" {
+            let file = File::open(fname);
+            if file.is_err() {
+                println!("Warning ({}): {}", fname, file.unwrap_err());
+                return Parser { input_stream: Box::new(iter::empty::<String>()) };
+            }
+            Box::new(file.unwrap())
+        } else {
+            Box::new(io::stdin())
+        };
+        let f = BufReader::new(reader);
+        Parser { input_stream: Box::new(f.lines().filter_map(|r| r.ok())) }
     }
+}
 
-    fn parse_file(self: &Parser, fname: &str) -> Vec<LogLine> {
+impl Iterator for Parser {
+    type Item = LogLine;
+
+
+    fn next(&mut self) -> Option<LogLine> {
         named!(test_start<&str>,
                ws!(do_parse!(take_until_and_consume_s!("TEST-START") >>
-                             tag!("|") >>
-                             path: not_line_ending >>
+                             tag!("|")                               >>
+                       path: not_line_ending                         >>
                              ( str::from_utf8(path).unwrap() ))));
 
         named!(parsed_number<u32>,
@@ -82,27 +100,15 @@ impl Parser {
             }
         }
 
-
-        let mut parsed = Vec::new();
-        let reader : Box<io::Read> = if fname != "-" {
-            let file = File::open(fname);
-            if file.is_err() {
-                println!("Warning ({}): {}", fname, file.unwrap_err());
-                return parsed;
-            }
-            Box::new(file.unwrap())
-        } else {
-            Box::new(io::stdin())
-        };
-        let f = BufReader::new(reader);
-        for line in f.lines().filter_map(|r| r.ok()) {
+        while let Some(line) = self.input_stream.next() {
             let bytes = line.into_bytes();
             if let IResult::Done(rest, path) = test_start(&bytes[..]) {
                 assert!(rest.len() == 0);
 
                 let last_slash = last_slash_idx(&path);
-                parsed.push(LogLine::TestStart(String::from(&path[last_slash..])));
-            } else if let IResult::Done(rest, (idx, loc)) = stack_component(&bytes[..]) {
+                return Some(LogLine::TestStart(String::from(&path[last_slash..])));
+            }
+            if let IResult::Done(rest, (idx, loc)) = stack_component(&bytes[..]) {
                 assert!(rest.len() == 0);
 
                 let lineno_sep = loc.rfind(':').unwrap();
@@ -110,47 +116,30 @@ impl Parser {
                 let fname = String::from(&path[last_slash_idx(&path)..]);
                 let line_no = loc[lineno_sep + 1..].parse::<u32>().unwrap();
 
-                parsed.push(LogLine::StackComponent(idx, path, fname, line_no));
+                return Some(LogLine::StackComponent(idx, path, fname, line_no));
             }
         }
 
-        parsed
+        None
     }
 }
 
 struct CPOWFinder<'a> {
-    idx: usize,
-    lines: &'a [LogLine],
-    peeked: bool,
+    parser: &'a mut Parser,
+    cur_line: Option<LogLine>,
     include_shims: bool,
 }
 
 impl<'a> CPOWFinder<'a> {
     // Returns the next line and leaves it.
-    fn peek_line(&mut self) -> Option<&'a LogLine> {
-        return if self.idx < self.lines.len() {
-            self.peeked = true;
-            Some(&self.lines[self.idx])
-        } else {
-            None
-        };
-    }
-
-    // "takes" the previously peeked line.
-    fn take_line(&mut self) {
-        assert!(self.peeked, "take without corresponding peek");
-        self.peeked = false;
-        self.idx += 1;
-    }
-
-    // Combination peek + take if we know there's at least one line.
-    fn next_line(&mut self) -> &'a LogLine {
-        if let Some(ref line) = self.peek_line() {
-            self.take_line();
-            return line;
+    fn next_line(&mut self) -> bool {
+        if let Some(line) = self.parser.next() {
+            self.cur_line = Some(line);
+            return true;
         }
 
-        panic!("shouldn't be out of lines");
+        self.cur_line = None;
+        return false;
     }
 
     // Given a CPOW usage, returns information about the CPOW if it is from
@@ -164,7 +153,7 @@ impl<'a> CPOWFinder<'a> {
         let mut report = false; // only report CPOWs from this test.
         let mut cpow = CPOW { line_no: 0, shim: false };
         let mut indirect_cpow = IndirectCPOW { line_no: 0, shim: false, filename: String::new() };
-        match self.next_line() {
+        match self.cur_line.as_ref().unwrap() {
             &LogLine::StackComponent(idx, ref path, ref filename, line_no) => {
                 assert!(idx == 0);
                 if is_test_path(path) {
@@ -187,12 +176,11 @@ impl<'a> CPOWFinder<'a> {
             }
         };
 
-        while let Some(next_line) = self.peek_line() {
-            match next_line {
+        while self.next_line() {
+            match self.cur_line.as_ref().unwrap() {
                 // Pull lines until we find the next test or the next CPOW.
                 &LogLine::StackComponent(0, _, _, _) | &LogLine::TestStart(_) => break,
                 &LogLine::StackComponent(_, ref path, ref filename, line_no) => {
-                    self.take_line();
                     if (!report || cpow.line_no == 0) && is_test_path(path) {
                         report = true;
 
@@ -230,10 +218,14 @@ impl<'a> CPOWFinder<'a> {
     fn parse_test(&mut self, testname: &str) -> Option<Test> {
         let mut cpows = Vec::new();
         let mut indirect_cpows = Vec::new();
-        while let Some(next_line) = self.peek_line() {
-            match next_line {
-                &LogLine::TestStart(_) => break,
-                &LogLine::StackComponent(_, _, _, _) => {
+        if !self.next_line() {
+            return None;
+        }
+
+        loop {
+            match self.cur_line.as_ref() {
+                None | Some(&LogLine::TestStart(_)) => break,
+                Some(&LogLine::StackComponent(_, _, _, _)) => {
                     match self.parse_cpow(testname) {
                         Some(SomeCPOW::CPOW(c)) => {
                             if !c.shim || self.include_shims {
@@ -262,16 +254,20 @@ impl<'a> CPOWFinder<'a> {
     }
 
     // Returns a list of tests that have CPOW uses.
-    fn compile_cpows(lines: &[LogLine], include_shims: bool) -> Vec<Test> {
-        let mut finder = CPOWFinder { idx: 0, lines: lines, peeked: false, include_shims: include_shims };
+    fn compile_cpows(mut parser: &mut Parser, include_shims: bool) -> Vec<Test> {
+        let mut finder = CPOWFinder { parser: &mut parser, cur_line: None, include_shims: include_shims };
         let mut tests = Vec::new();
-        while let Some(next_line) = finder.peek_line() {
-            finder.take_line();
-            match next_line {
-                &LogLine::StackComponent(..) => {
+        if !finder.next_line() {
+            return tests;
+        }
+
+        loop {
+            match finder.cur_line.clone() {
+                None => break,
+                Some(LogLine::StackComponent(..)) => {
                     panic!("unconsumed StackComponent?");
                 }
-                &LogLine::TestStart(ref fname) => {
+                Some(LogLine::TestStart(ref fname)) => {
                     if let Some(test) = finder.parse_test(fname) {
                         tests.push(test);
                     }
@@ -298,13 +294,12 @@ fn main() {
         println!("Including CPOWs via shims. Indirect CPOWs via a shim are marked by a leading *.\n");
     }
 
-    let m = Parser::new();
     let mut all_tests = BTreeMap::new();
     let mut num_cpows = 0;
 
     for fname in matches.values_of("FILES").unwrap() {
-        let p = m.parse_file(&fname);
-        let tests = CPOWFinder::compile_cpows(p.as_slice(), include_shims);
+        let mut p = Parser::new(fname);
+        let tests = CPOWFinder::compile_cpows(&mut p, include_shims);
 
         for test in tests {
             num_cpows += test.cpows.len() + test.indirect_cpows.len();
